8e229c6b7557e532debc81eee380de39f8471988
Luciano1438 <luchoarmas28@gmail.com>
2026-02-08 21:09:47 -0300

    app.py

--- src/app.py
+++ src/app.py
@@ -22,7 +22,7 @@ def generar_pdf_presupuesto(datos):
     pdf.set_font("Arial", '', 10)
     tz_arg = timezone(timedelta(hours=-3))
     fecha_hoy = datetime.now(tz_arg).strftime('%d/%m/%Y')
-    pdf.cell(200, 10, f"Fecha de emisión: {fecha_hoy}", ln=True, align='R')    pdf.ln(10)
+    pdf.cell(200, 10, f"Fecha de emisión: {fecha_hoy}", ln=True, align='R')    
 
     # Cuerpo del Presupuesto
     pdf.set_font("Arial", 'B', 12)
@@ -493,6 +493,7 @@ elif menu == "⚙️ Configuración de Precios":
         actualizar_precio_nube('colocacion_dia', config['colocacion_dia'])
         
         st.success("Configuración blindada en Supabase para todos los parámetros.")
+
 
 
 
