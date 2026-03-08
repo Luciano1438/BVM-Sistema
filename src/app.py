0642eeb9e2b6589c8ff868a01c405454643e3c3b
Luciano1438 <luchoarmas28@gmail.com>
2026-03-08 17:55:24 -0300

    app.py

--- src/app.py
+++ src/app.py
@@ -275,6 +275,7 @@ if menu == "Cotizador CNC":
         costo_fondo = 0.0
         costo_herrajes = 0.0
         costo_flete = 0.0
+        precio_final = 0.0
         # --- DASHBOARD DE CONTROL ---
         st.write("---")
         # Calculamos la rentabilidad proyectada (usamos valores base si no hay datos)
@@ -788,6 +789,7 @@ if menu == "⚙️ Configuración de Precios" and st.session_state["user_data"][
                     st.error(f"Error al crear cuenta: {e}")
             else:
                 st.warning("Completá usuario y contraseña para continuar.")
+
 
 
 
