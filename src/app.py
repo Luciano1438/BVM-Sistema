54c1df0110e45115fc3da05ca6dd3075b853b7db
Luciano1438 <luchoarmas28@gmail.com>
2026-03-08 18:01:11 -0300

    app.py

--- src/app.py
+++ src/app.py
@@ -277,6 +277,9 @@ if menu == "Cotizador CNC":
         costo_flete = 0.0
         precio_final = 0.0
         mueble_nom = "Mueble BVM"
+        tiene_parante = False        # <--- Culpable 1
+        distancia_parante = 0.0      # <--- Culpable 2
+        cant_estantes = 0
         # --- DASHBOARD DE CONTROL ---
         st.write("---")
         # Calculamos la rentabilidad proyectada (usamos valores base si no hay datos)
@@ -792,6 +795,7 @@ if menu == "⚙️ Configuración de Precios" and st.session_state["user_data"][
                     st.error(f"Error al crear cuenta: {e}")
             else:
                 st.warning("Completá usuario y contraseña para continuar.")
+
 
 
 
