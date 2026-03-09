e6f06cb4ec834fb81c6cfc1992ba3fcb7b1a6b74
Luciano1438 <luchoarmas28@gmail.com>
2026-03-08 21:34:31 -0300

    app.py

--- src/app.py
+++ src/app.py
@@ -158,7 +158,7 @@ def generar_despiece_bvm(tipo, ancho_m, alto_m, prof_m, esp_real, tiene_parante,
                  despiece.append({"Pieza": "Frentín Frontal", "Cant": 1, "L": ancho_interno_total, "A": 50, "Tipo": "Cuerpo"})
                  despiece.append({"Pieza": "Travesaño Superior", "Cant": 1, "L": ancho_interno_total, "A": 100, "Tipo": "Cuerpo"})
                  alto_puerta = alto_m - 4
-             elif tipo_tapa == "Gola BVM":
+            elif tipo_tapa == "Gola BVM":
                  despiece.append({"Pieza": "Frentín Gola L (A)", "Cant": 1, "L": ancho_interno_total, "A": 40, "Tipo": "Cuerpo"})
                  despiece.append({"Pieza": "Frentín Gola L (B)", "Cant": 1, "L": ancho_interno_total, "A": 50, "Tipo": "Cuerpo"})
                  alto_puerta = alto_m - 30
@@ -843,6 +843,7 @@ if menu == "⚙️ Configuración de Precios" and st.session_state["user_data"][
                     st.error(f"Error al crear cuenta: {e}")
             else:
                 st.warning("Completá usuario y contraseña para continuar.")
+
 
 
 
