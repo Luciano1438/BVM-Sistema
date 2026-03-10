e655765f026e1211c2401b074cb2acfb49539370
Luciano1438 <luchoarmas28@gmail.com>
2026-03-10 13:37:58 -0300

    app.py

--- src/app.py
+++ src/app.py
@@ -394,7 +394,8 @@ if menu == "Cotizador CNC":
         costo_fondo = 0.0
         costo_herrajes = 0.0
         costo_flete = 0.0
-        precio_final = 0.
+        precio_final = 0.0
+        total_costo = 0.0
         tiene_parante = False
         mueble_nom = "Mueble BVM"
         tiene_parante = False
@@ -865,6 +866,7 @@ if menu == "⚙️ Configuración de Precios" and st.session_state["user_data"][
                     st.error(f"Error al crear cuenta: {e}")
             else:
                 st.warning("Completá usuario y contraseña para continuar.")
+
 
 
 
