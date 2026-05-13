d8ef89ccfd0446557db791c2fa50502bc431d6a8
Luciano1438 <luchoarmas28@gmail.com>
2026-05-13 01:02:29 -0300

    Import functions from despiece, retazos, and exportadores

--- src/motor/__init__.py
+++ src/motor/__init__.py
@@ -1,1 +1,4 @@
 # motor/__init__.py
+from .despiece import generar_despiece_bvm, obtener_veta_automatica, calcular_medida_frente
+from .retazos import es_retazo_util, pieza_entra_en_retazo, calcular_ahorro_retazos
+from .exportadores import generar_pdf_presupuesto, generar_dxf_bvm, exportar_para_aspire, generar_link_whatsapp
